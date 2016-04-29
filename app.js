8e429606b46351ac7f98527461237ec014a1a70a
Diego Figueroa <diego.fv94@gmail.com>
2016-04-29 13:09:54 -0400

    add newPost and incrementLikes functions

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var app = angular.module('postMe', []);
 app.controller('MainCtrl', ['$scope', function ($scope) {
 	$scope.test = 'Hello World!';
 	$scope.posts = [
-		'post 1',
-		'post 2',
-		'post 3',
-		'post 4',
-		'post 5'
-	]
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 2},
+		{title: 'post 3', upvotes: 5},
+		{title: 'post 4', upvotes: 10},
+		{title: 'post 5', upvotes: 9}
+	];
+	$scope.addPost = function(){
+		if(!$scope.title | $scope.title === ''){return; }
+		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.title = ''
+	};
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	};
 }]);
