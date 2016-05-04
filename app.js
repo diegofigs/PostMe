ac6c3f78a8f12eac06d95f2c131465716e53e5c0
Diego Figueroa <diego.fv94@gmail.com>
2016-05-04 14:16:20 -0400

    add passport and dotenv modules for authentication

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var dotenv = require('dotenv');
+dotenv.config();
+
 var mongoose = require('mongoose');
+
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 mongoose.connect('mongodb://localhost/news')
 
@@ -27,6 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
